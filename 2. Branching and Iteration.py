0d653077c6ee5d1579f8adb111dd638cb5d0dfac
avvarimanju <avvarimanju@gmail.com>
2018-03-19 16:03:35 +0530

    Initial files

---
+++ 2. Branching and Iteration.py
@@ -0,0 +1,24 @@
+
+"""
+STRINGS
+"""
+
+hi = "hello there"
+print (hi)
+name = "Manju"
+print (name)
+
+# Concatenate Strings 
+greet = hi + name
+print(greet)
+greeting = hi + " " + name
+print(greeting)
+
+silly1 = hi + " " + name * 3
+print(silly1)
+silly2 = hi*3
+print(silly2)
+
+print("silly2")
+print(silly2*3)
+
